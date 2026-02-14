ffa4eae4ff9b24fa136cf66078be169e491d503a
hellseye <brutalyso@gmail.com>
2026-02-14 21:10:10 +0300

    777

--- js/app.js
+++ js/app.js
@@ -120,8 +120,8 @@ function renderPage(page) {
       container.innerHTML = `
         <h1>Подписки</h1>
 
-        <div class="card profile-card">
-          <div>
+        <div class="card subscription-card clickable">
+          <div class="subscription-content">
             <strong>${subscriptionData.subscription_type.toUpperCase()}</strong>
             <div class="hint">
               Действует до: ${new Date(
@@ -129,9 +129,12 @@ function renderPage(page) {
               ).toLocaleDateString()}
             </div>
           </div>
-          <button class="secondary-btn" style="width:auto; margin:0;">
-            Настроить
-          </button>
+
+          <div class="gear-bg">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+              <path fill-rule="evenodd" d="M7.84 1.804A1 1 0 0 1 8.82 1h2.36a1 1 0 0 1 .98.804l.331 1.652a6.993 6.993 0 0 1 1.929 1.115l1.598-.54a1 1 0 0 1 1.186.447l1.18 2.044a1 1 0 0 1-.205 1.251l-1.267 1.113a7.047 7.047 0 0 1 0 2.228l1.267 1.113a1 1 0 0 1 .206 1.25l-1.18 2.045a1 1 0 0 1-1.187.447l-1.598-.54a6.993 6.993 0 0 1-1.929 1.115l-.33 1.652a1 1 0 0 1-.98.804H8.82a1 1 0 0 1-.98-.804l-.331-1.652a6.993 6.993 0 0 1-1.929-1.115l-1.598.54a1 1 0 0 1-1.186-.447l-1.18-2.044a1 1 0 0 1 .205-1.251l1.267-1.114a7.05 7.05 0 0 1 0-2.227L1.821 7.773a1 1 0 0 1-.206-1.25l1.18-2.045a1 1 0 0 1 1.187-.447l1.598.54A6.992 6.992 0 0 1 7.51 3.456l.33-1.652ZM10 13a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" clip-rule="evenodd" />
+            </svg>
+          </div>
         </div>
       `;
     }
